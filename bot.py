71ee74f66bbf636daa35271521d0a43d44d914ee
takeraparterer <96804457+takeraparterer@users.noreply.github.com>
2022-12-17 15:47:18 +1000

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import discord
 from discord.ext import commands
+import re
 from aitextgen.TokenDataset import TokenDataset
 from aitextgen.tokenizers import train_tokenizer
 from aitextgen.utils import GPT2ConfigCPU
 from aitextgen import aitextgen
 import textwrap
-ai2 = aitextgen(model_folder="absolute_path_to_file/trained_model",
-tokenizer_file="absolute_path_to_file/aitextgen.tokenizer.json")
+ai2 = aitextgen(model_folder="path/to/file/trained_model",
+tokenizer_file="path/to/file/aitextgen.tokenizer.json")
+pattern = r"<@(.*?)>"
+def has_letters(string):
+  # Use a regular expression to check if the string contains any letters
+  if re.search(r'[a-zA-Z]', string):
+    return True
+  else:
+    return False
 def Average(lst):
     return sum(lst) / len(lst)
 def getspamness(instr):
@@ -23,10 +31,15 @@ def checkspam(fullin):
     #print(chunks)
 
     wrapstr=chunks
-    while len(wrapstr[len(wrapstr)-1]) <8:
-        wrapstr[len(wrapstr)-1] = wrapstr[len(wrapstr)-1] + "0"
+    #while len(wrapstr[len(wrapstr)-1]) <8:
+    #    wrapstr[len(wrapstr)-1] = wrapstr[len(wrapstr)-1] + "0"
+    if len(wrapstr[len(wrapstr)-1]) <8:
+      del wrapstr[-1]
+    if wrapstr == []:
+      wrapstr = ["hello te"]
     base = []
     for i in range(len(wrapstr)):
+        print(getspamness(wrapstr[i]))
         base.append(int(getspamness(wrapstr[i])[-2]))
     print("average spam amount is: "+str(Average(base)))
     return int(Average(base))
@@ -35,11 +48,30 @@ bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 @bot.event
 async def on_message(message):
   # Check if message is longer than 1000 characters\
-  print("on_message! message.content len is: "+str(len(message.content)))
+  print("on_message! message.content is: "+str(message.content))
   if len(message.content) > 4:
-    if checkspam(str(message.content)) >=4:
+    string = message.content
+    match = re.search(pattern, string)
+
+    if match:
+        print("stripping pings...")
+        print(string.replace(match.group(0), ""))
+        print(has_letters(match.group(0)))
+        print(len(match.group(0)))
+        if not len(match.group(0)) > 22:
+          
+          if not has_letters(match.group(0)):
+            string = string.replace(match.group(0), "")
+        if string.replace(" ","") == "":
+          string = "hello te"
+    try:
+      if checkspam(str(string.lower())) >=4:
+        print(checkspam(str(string.lower())))
+        if True:
+          await message.delete()
+          print("deleted message")
+    except:
       await message.delete()
-      print("deleted message")
     # Count number of different character types in message
     #num_char_types = len(set(message.content))
     ## Check if there are less than 6 different character types
@@ -48,4 +80,4 @@ async def on_message(message):
     #  await message.delete()
     #  print("deleted message")
 
-bot.run('token')
+bot.run('TOKEN')
